f3d8b4dd09fc5439dbafdf504bb9c0f7d013aced
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-04-26 19:50:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2_ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b, a, t;
+
+
+            Console.Write("Digite o valor de a1: ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.Write("digite o  valor de b1: ");
+            a = double.Parse(Console.ReadLine());
+            t = b * a;
+            if (t > 100)
+                Console.Write("terreno grande: " + t );
+            else
+                Console.Write(t);
+
+
+        }
+    }
+}
